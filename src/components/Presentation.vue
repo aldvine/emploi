182b2694c9312f8d0fecd01b7c6608bab816fa91
Aldvine Blanchard <aldvine.blanchard@gmail.com>
2018-11-02 15:59:50 +0100

    correction faute

--- src/components/Presentation.vue
+++ src/components/Presentation.vue
@@ -4,8 +4,7 @@
             <h1 class="display-4">L'emploi en France</h1>
             <p class="lead">Grâce au projet sur l'open data, nous avons choisi d'analyser l'emploi en France. </p>
             <hr class="my-4">
-            <p>Nous nous sommes appuyé principalement sur les données de Pôle Emploi.
-                Malgré que c'est un sujet qui revient beaucoup, on retrouve très peu de données concernant ce sujet.</p>
+            <p>Nous nous sommes appuyés principalement sur les données de Pôle Emploi.</p>
            
         </div>
     </div>
